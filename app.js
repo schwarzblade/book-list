c66f7de587a46efc04235d40a3af0e9379fed91f
luk <schwarzblade@gamil.com>
2020-04-08 12:27:23 +0200

    set up project

---
+++ app.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+	<likn rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<title>Book Lst</title>
+</head>
+<body>
+<h1>cos tam</h1>
+
+</body>
+</html> 
